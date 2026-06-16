b8c3191d795c19b14e8075dc3329ff02d121e126
Cuong Liam <cuongdeptrai2706@gmail.com>
2026-06-16 07:36:33 +0700

    init

---
+++ src/main/java/com/example/hkt1/Hkt1Application.java
@@ -0,0 +1,13 @@
+package com.example.hkt1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hkt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hkt1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hkt1/Hkt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hkt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hkt1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
